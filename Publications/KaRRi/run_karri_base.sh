6a9eb8e66bdaef4a50335c9d1ecbb1fca4632c52
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-06-12 11:20:04 +0200

    Add support for the different filter strategies

--- Publications/KaRRi/run_karri_base.sh
+++ Publications/KaRRi/run_karri_base.sh
@@ -20,6 +20,10 @@ outputBaseDir=$3
 radius=$4
 timeout=$5
 
+# Farben für Konsolen-Output
+green='\033[0;32'
+no_color='\033[0m'
+
 # Prüfe, ob Output Directory existiert
 if ! [ -d "$outputBaseDir" ]; then
 	echo "Output directory ${outputBaseDir} does not exist."
@@ -46,9 +50,9 @@ sepDecomp=$inputDir/SepDecomps/${instanceName}_nd30.sep.bin
 # Defines for different Filter Strategies
 heuristics=("ALL" "MAX_RAND" "CH_ABS" "CH_REL" "PARETO_SIMPLE" "PARETO_DIR")
 
-# Erzeuge konkretes Output-Directory, dessen Name aus instanceName + aktuellem timestamp besteht.
+# Erzeuge konkretes Output-Directory, dessen Name aus instanceName + radius + aktuellem timestamp besteht.
 currentTime=$(date "+%Y.%m.%d-%H:%M")
-karriOutputDir=$outputBaseDir/LOUD/${instanceName}_${currentTime}
+karriOutputDir=$outputBaseDir/LOUD/${instanceName}_${radius}_${currentTime}
 mkdir -p $karriOutputDir
 
 # Baue KaRRi nach $karriSourceDir/Build/Release; gib explizit hard-gecodeten Ort der dependencies an.
@@ -72,22 +76,122 @@ cmake --build ${karriBinaryDir}_${strat} --target karri -j 16
 
 done
 
+echo -e "${green}Starting Test Runs${no_color}"
+# Lasse KaRRi für jede Strategie 5 Mal laufen (nur relevant für Laufzeitmessungen, für Qualität reicht ein Run)
 
-# Lasse KaRRi 5 Mal laufen (nur relevant für Laufzeitmessungen, für Qualität reicht ein Run)
+echo -e "${green}Starting Runs with Filter Strategy ALL${no_color}"
+# 1: All PD Locs
 for i in {1..5}
-do
-
-for strat in ${heuristics[@]}
 do
 
 # Run pedestrian/KaRRi, radius 300, wait time 300
 # ID, um zwischen 5 runs zu unterscheiden
 run_id=KaRRi_run$i
-timeout $timeout taskset 0x1 ${karriBinaryDir}_$strat/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/${run_id}_$strat
+timeout $timeout taskset 0x1 ${karriBinaryDir}_ALL/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/ALL_${run_id}
+
+done
+
+echo -e "${green}Done with Runs with Filter Strategy ALL${no_color}"
+echo -e "${green}Starting Runs with Filter Strategy MAX_RAND${no_color}"
+
+# 2: Maximum of k randomly chosen PD Locs, k in {1,2,3,4,5,10,15,20,25}
+kValues=(1 2 3 4 5 10 15 20 25)
+for k in ${kValues[@]}
+do
+
+for i in {1..5}
+do
+
+# Run pedestrian/KaRRi, wait time 300
+# ID, um zwischen 5 runs zu unterscheiden
+run_id=KaRRi_run$i
+timeout $timeout taskset 0x1 ${karriBinaryDir}_MAX_RAND/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/MAX_RAND_$k_${run_id} -max-num-d $k
 
 done
 
 done
+
+echo -e "${green}Done with Runs with Filter Strategy MAX_RAND${no_color}"
+echo -e "${green}Starting Runs with Filter Strategy CH_ABS${no_color}"
+
+# 2: Maximum of k PD Locs, picking those with the highest rank in the CH, k in {1,2,3,4,5,10,15,20,25}
+kValues=(1 2 3 4 5 10 15 20 25)
+for k in ${kValues[@]}
+do
+
+for i in {1..5}
+do
+
+# Run pedestrian/KaRRi, wait time 300
+# ID, um zwischen 5 runs zu unterscheiden
+run_id=KaRRi_run$i
+timeout $timeout taskset 0x1 ${karriBinaryDir}_CH_ABS/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/CH_ABS_$k_${run_id} -max-num-d $k
+
+done
+
+done
+
+echo -e "${green}Done with Runs with Filter Strategy CH_ABS${no_color}"
+echo -e "${green}Starting Runs with Filter Strategy CH_REL${no_color}"
+
+# 2: Pick all PD Locs whose rank is with in k percent of the rank of the PD Loc with the highest rank in the radius, k in {1,2,3,4,5,6,8,10,12,15,20}
+kValues=(1 2 3 4 5 6 8 10 12 15 20)
+for k in ${kValues[@]}
+do
+
+for i in {1..5}
+do
+
+# Run pedestrian/KaRRi, wait time 300
+# ID, um zwischen 5 runs zu unterscheiden
+run_id=KaRRi_run$i
+timeout $timeout taskset 0x1 ${karriBinaryDir}_CH_REL/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/CH_REL_$k_${run_id} -max-num-d $k
+
+done
+
+done
+
+echo -e "${green}Done with Runs with Filter Strategy CH_REL${no_color}"
+echo -e "${green}Starting Runs with Filter Strategy PARETO_SIMPLE${no_color}"
+
+# 2: Pick all PD Locs that are not dominated by more than k PD Locs both in terms of CH rank and in terms of distance from the origin, k in {1,2,3,4,5,6,7}
+kValues=(1 2 3 4 5 6 7)
+for k in ${kValues[@]}
+do
+
+for i in {1..5}
+do
+
+# Run pedestrian/KaRRi, wait time 300
+# ID, um zwischen 5 runs zu unterscheiden
+run_id=KaRRi_run$i
+timeout $timeout taskset 0x1 ${karriBinaryDir}_PARETO_SIMPLE/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/PARETO_SIMPLE_$k_${run_id} -max-num-d $k
+
+done
+
+done
+
+echo -e "${green}Done with Runs with Filter Strategy PARETO_SIMPLE${no_color}"
+echo -e "${green}Starting Runs with Filter Strategy PARETO_DIR${no_color}"
+
+# 2: Pick all PD Locs that are not dominated by more than k PD Locs both in terms of CH rank and in terms of distance from the origin and make sure that in every octant there are at least k PD Locs (if possible), k in {1,2,3,4,5,6,7}
+kValues=(1 2 3 4 5 6 7)
+for k in ${kValues[@]}
+do
+
+for i in {1..5}
+do
+
+# Run pedestrian/KaRRi, wait time 300
+# ID, um zwischen 5 runs zu unterscheiden
+run_id=KaRRi_run$i
+timeout $timeout taskset 0x1 ${karriBinaryDir}_PARETO_DIR/Launchers/karri -w 300 -p-radius $radius -d-radius $radius -veh-g $vehGraph -psg-g $psgGraph -v $vehicles -r $requests -veh-h $vehCh -psg-h $psgCh -o $karriOutputDir/PARETO_DIR_$k_${run_id} -max-num-d $k
+
+done
+
+done
+
+echo -e "${green}Done with Runs with Filter Strategy PARETO_DIR${no_color}"
 
 # Bemerkung:
 #
